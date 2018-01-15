128ec3e7068533ac94d4b41c6532afe936f6150b
ZhangRan <mail@zhangran.name>
2018-01-15 22:21:17 +0800

    选择排序

---
+++ selection_sort.py
@@ -0,0 +1,20 @@
+# fist find out the smallest
+def findSmallest(arr):
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+    return smallest_index
+
+# now def selection sort function
+def selectionSort(arr):
+    newArr = []
+    for i in range(len(arr)):
+        smallest_index = findSmallest(arr)
+        newArr.append(arr.pop(smallest_index))
+    return newArr
+
+# test
+print selectionSort([5,3,6,2,10])
